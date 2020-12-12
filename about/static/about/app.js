7541fa9765eb072863901eaf8f22d466ba66adc8
Itsuki <t.itsuki928@gmail.com>
2020-12-12 23:35:51 +0900

    Updated about page

--- about/static/about/app.js
+++ about/static/about/app.js
@@ -1,23 +1,63 @@
 
 const card = document.querySelector('.card')
 const selector = document.querySelector('#section');
-const cardTitle = document.querySelector('.card-title');
-const cardText = document.querySelector('.card-text');
-const cardLink = document.querySelector('.card-link');
+const text = document.querySelector('#test');
 
 selector.addEventListener('change', (e) => {
-    if(e.target.value !== ''){
-        let section = e.target.value;
-        cardTitle.textContent = section;
-        cardLink.textContent = section;
-        section = section.toLowerCase();
-                 <a href="{% url 'portfolio' %}" class="nav-link">Portfolio</a> 
-        cardLink.href = `{% url '${section}' %}`;
-        // section = section.toLowerCase();
-        // a.setAttribute('href', `{% url "${section}/${section}.html" %}`)
-        // a.setAttribute('href', '{% url "hellooo" %}')
+    if (e.target.value === 'Website') {
+        card.innerHTML = `
+                <div class="card-header" id="headingOne">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Frontend
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div class="card-body">
+                        HTML&CSS
+                        Javascript
+                    </div>
+                </div>
+                <div class="card-header" id="headingTwo">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                            Backend
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div class="card-body">
+                        Django
+                    </div>
+                </div>
+                <div class="card-header" id="headingTree">
+                    <h5 class="mb-0">
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTree" aria-expanded="true" aria-controls="collapseTree">
+                            The Other 
+                        </button>
+                    </h5>
+                </div>
+                <div id="collapseTree" class="collapse show" aria-labelledby="headingTree" data-parent="#accordion">
+                    <div class="card-body">
+                        Docker 
+                        AWS
+                    </div>
+                </div>
+            
+        `;
         card.style.display = 'block';
-    }else{
+    } else if (e.target.value === 'Creator') {
+        card.innerHTML = `
+             <img id="picture-of-me" src="${img}" alt="picture-of-me">
+             <div class="card-body">
+                <h5 class="card-title">Itsuki Tomizawa</h5>
+                <p class="card-text">Hi! I'm Itsuki. <br>
+                I am in my third year of studying computer science at California State University Chico to become a software engineer.</p>
+             </div>
+        `;
+        card.style.display = 'block';
+    } else {
         card.style.display = 'none';
     }
 });
